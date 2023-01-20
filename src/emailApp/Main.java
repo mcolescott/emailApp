156f6fe154b6902e46e143487847da6431bfb913
mcolescott <matthewcolescott@gmail.com>
2023-01-19 19:22:59 -0800

    Initial commit

---
+++ src/emailApp/Main.java
@@ -0,0 +1,13 @@
+package emailApp;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // Generate an email with the following syntax: firstname.lastname@department.company.com
+        // Determine the department (sales, development, accounting), if none, leave blank
+        // Generate a random string for a password
+        // Have set methods to change the password, set the mailbox capacity, and define an alternate email address
+        // Have get methods to display the name, email, and mailbox capacity
+
+    }
+}
